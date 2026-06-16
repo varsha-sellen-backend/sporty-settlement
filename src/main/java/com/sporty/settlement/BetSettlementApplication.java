139dfc83b1373d4f25ec7f00b981ec3fdba9cd22
Varsha Sellen <varshasellen@Varshas-MacBook-Air.local>
2026-06-16 13:51:39 +0200

    init: spring boot project with h2 and jpa dependencies

---
+++ src/main/java/com/sporty/settlement/BetSettlementApplication.java
@@ -0,0 +1,12 @@
+package com.sporty.settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetSettlementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetSettlementApplication.class, args);
+    }
+}
